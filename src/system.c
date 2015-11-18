daf6040212f941a7e4778fcccdefcf07a885d245
Thiago Scalone <thiago@scalone.com.br>
2015-11-18 15:39:15 -0200

    Fix hwclock year definition.

--- src/system.c
+++ src/system.c
@@ -81,7 +81,7 @@ mrb_system_s_hwclock(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "iiiiii", &year, &month, &day, &hour, &minute, &second);
 
-  stRTC.bYear   = year;
+  stRTC.bYear   = year-100;
   stRTC.bMonth  = month;
   stRTC.bDay    = day;
   stRTC.bHour   = hour;
