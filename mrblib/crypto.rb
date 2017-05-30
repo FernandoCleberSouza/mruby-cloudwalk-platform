1f41c370914e594a8dde3df816a41d9a6192654f
Thiago Scalone <thiago@scalone.com.br>
2017-05-30 14:22:51 -0300

    Refactoring Platform::Crypto.dukpt_encrypt_buffer

--- mrblib/crypto.rb
+++ mrblib/crypto.rb
@@ -1,8 +1,5 @@
 class Platform::Crypto
   def self.dukpt_encrypt_buffer(slot, message)
-    ret, out, ksn = self._dukpt_encrypt_buffer(slot, message)
-    if ret == 0
-      [ret, out.to_s.upcase, ksn.to_s.upcase]
-    end
+    self._dukpt_encrypt_buffer(slot, message)
   end
 end
